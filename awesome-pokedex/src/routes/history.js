0603fe6ab6e13d144f377debd144e2b8d9b23799
Leticia Monteiro <leticiamonteiro.design@gmail.com>
2019-03-27 13:21:22 -0300

    try to fix deploy

--- awesome-pokedex/src/routes/history.js
+++ awesome-pokedex/src/routes/history.js
@@ -1,5 +1,5 @@
 import { createBrowserHistory } from 'history';
 
-const history = createBrowserHistory({ basename: '/awesome-pokedex/' });
+const history = createBrowserHistory({ basename: '/awesome-pokedex/awesome-pokedex/' });
 
 export default history;
