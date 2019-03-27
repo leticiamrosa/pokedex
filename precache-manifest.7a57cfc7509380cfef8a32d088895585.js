27e291305cba8eabc439f527dbd7376e8678b550
Leticia Monteiro <leticiamonteiro.design@gmail.com>
2019-03-27 11:19:00 -0300

    Updates

---
+++ precache-manifest.7a57cfc7509380cfef8a32d088895585.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1734472e51c8511874b7",
+    "url": "/awesome-pokedex/awesome-pokedex/static/js/runtime~main.c83b5fad.js"
+  },
+  {
+    "revision": "f6a2d1fd1de41a22f4cf",
+    "url": "/awesome-pokedex/awesome-pokedex/static/js/main.d5a6ae5e.chunk.js"
+  },
+  {
+    "revision": "b98a4b7c5cfd66319821",
+    "url": "/awesome-pokedex/awesome-pokedex/static/js/2.6bac486b.chunk.js"
+  },
+  {
+    "revision": "4555d2284063f35764ec9f2f1e689aeb",
+    "url": "/awesome-pokedex/awesome-pokedex/index.html"
+  }
+];
