4254091ac73a3340ad04352468ea8632f45a8b79
Leticia Monteiro <leticiamonteiro.design@gmail.com>
2019-07-14 06:31:31 -0300

    Updates

--- awesome-pokedex/src/routes/history.js
+++
@@ -1,5 +0,0 @@
-import { createBrowserHistory } from 'history';
-
-const history = createBrowserHistory({ basename: '/pokedex/' });
-
-export default history;
---
+++ precache-manifest.95c2e41c8a45137845cf26db55d577c6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "9d500717c5c4b6589fe3",
+    "url": "/pokedex/static/js/main.80b1824d.chunk.js"
+  },
+  {
+    "revision": "024a09093cd901ecc16a",
+    "url": "/pokedex/static/js/2.0d5dfb0b.chunk.js"
+  },
+  {
+    "revision": "435a3c9ddb8ace9149d9a4584f6502bb",
+    "url": "/pokedex/index.html"
+  }
+];
